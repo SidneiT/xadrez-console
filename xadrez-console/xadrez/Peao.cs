4fcdbaac804cc5b02414f1eb4560de5617b3c65e
Sidnei Teixeira <sidnei.teixeira@outlook.com>
2018-01-21 17:58:19 -0200

    Jogada especial roque pequeno e grande

--- xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez/Peao.cs
@@ -4,9 +4,9 @@ namespace xadrez {
 
     class Peao : Peca {
 
-        private PartidaDeXadrez partida;
+        private PartidaXadrez partida;
 
-        public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor) {
+        public Peao(Tabuleiro tab, Cor cor, PartidaXadrez partida) : base(tab, cor) {
             this.partida = partida;
         }
 
@@ -15,77 +15,77 @@ namespace xadrez {
         }
 
         private bool existeInimigo(Posicao pos) {
-            Peca p = tab.peca(pos);
+            Peca p = tabuleiro.peca(pos);
             return p != null && p.cor != cor;
         }
 
         private bool livre(Posicao pos) {
-            return tab.peca(pos) == null;
+            return tabuleiro.peca(pos) == null;
         }
 
         public override bool[,] movimentosPossiveis() {
-            bool[,] mat = new bool[tab.linhas, tab.colunas];
+            bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
 
             Posicao pos = new Posicao(0, 0);
 
             if (cor == Cor.Branca) {
                 pos.definirValores(posicao.linha - 1, posicao.coluna);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tabuleiro.posicaoValida(pos) && livre(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 2, posicao.coluna);
                 Posicao p2 = new Posicao(posicao.linha - 1, posicao.coluna);
-                if (tab.posicaoValida(p2) && livre(p2) && tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
+                if (tabuleiro.posicaoValida(p2) && livre(p2) && tabuleiro.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                if (tabuleiro.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                if (tabuleiro.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
                 // #jogadaespecial en passant
                 if (posicao.linha == 3) {
                     Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
-                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant) {
+                    if (tabuleiro.posicaoValida(esquerda) && existeInimigo(esquerda) && tabuleiro.peca(esquerda) == partida.vulneravelEnPassant) {
                         mat[esquerda.linha - 1, esquerda.coluna] = true;
                     }
                     Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
-                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant) {
+                    if (tabuleiro.posicaoValida(direita) && existeInimigo(direita) && tabuleiro.peca(direita) == partida.vulneravelEnPassant) {
                         mat[direita.linha - 1, direita.coluna] = true;
                     }
                 }
             }
             else {
                 pos.definirValores(posicao.linha + 1, posicao.coluna);
-                if (tab.posicaoValida(pos) && livre(pos)) {
+                if (tabuleiro.posicaoValida(pos) && livre(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 2, posicao.coluna);
                 Posicao p2 = new Posicao(posicao.linha + 1, posicao.coluna);
-                if (tab.posicaoValida(p2) && livre(p2) && tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
+                if (tabuleiro.posicaoValida(p2) && livre(p2) && tabuleiro.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                if (tabuleiro.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                if (tabuleiro.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
                 // #jogadaespecial en passant
                 if (posicao.linha == 4) {
                     Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
-                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant) {
+                    if (tabuleiro.posicaoValida(esquerda) && existeInimigo(esquerda) && tabuleiro.peca(esquerda) == partida.vulneravelEnPassant) {
                         mat[esquerda.linha + 1, esquerda.coluna] = true;
                     }
                     Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
-                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant) {
+                    if (tabuleiro.posicaoValida(direita) && existeInimigo(direita) && tabuleiro.peca(direita) == partida.vulneravelEnPassant) {
                         mat[direita.linha + 1, direita.coluna] = true;
                     }
                 }
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -3,11 +3,13 @@
 namespace xadrez
 {
     class Rei : Peca
-    { 
+    {
 
-        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        private PartidaXadrez partida;
+
+        public Rei(Tabuleiro tabuleiro, Cor cor, PartidaXadrez partida) : base(tabuleiro, cor)
         {
-
+            this.partida = partida;
         }
 
 
@@ -22,6 +24,12 @@ namespace xadrez
             return p == null || p.cor != cor;
         }
 
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tabuleiro.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qtdMovimentos == 0;
+        }
+
         public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
@@ -29,7 +37,7 @@ namespace xadrez
             Posicao pos = new Posicao(0, 0);
 
             // acima
-            pos.definirValores(posicao.linha -1, posicao.coluna);
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
@@ -82,6 +90,35 @@ namespace xadrez
             if (tabuleiro.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
+            }
+
+            // jogada especial Roque
+            if (qtdMovimentos == 0 && !partida.xeque)
+            {
+                // roque pequeno
+                Posicao posT = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posT))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(p1 == null && p2 == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posT))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3  = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (p1 == null && p2 == null && p3 == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
